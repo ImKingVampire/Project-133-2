68a710c9f46afa921b059076fd386483645a7e2c
ImKingVampire <78542051+ImKingVampire@users.noreply.github.com>
2021-11-16 19:10:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function hh(){
+    window.location="hall.html";
+}
+
+function bedroom(){
+    window.location="bedroom.html";
+}
+
+function bottle(){
+    window.location="bottle.html";
+}
+
+function kitchen(){
+    window.location="kitchen.html";
+}
+
+function phone(){
+    window.location="phone.html";
+}
